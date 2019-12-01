c3775b633775807213c63508acd774fffcd8a49c
Warut Surapat <s.warut@gmail.com>
2019-12-01 23:09:35 +0900

    Add day 1 part 2.

--- day1/elixir/lib/day1.ex
+++ day1/elixir/lib/day1.ex
@@ -6,7 +6,7 @@ defmodule Day1 do
     |> Enum.map(fn x -> String.to_integer(x) end)
   end
 
-  def solve() do
+  def solve_part1() do
     sum_of_fuel(get_input(), 0)
   end
 
@@ -16,6 +16,28 @@ defmodule Day1 do
   end
 
   def sum_of_fuel([], fuel_sum) do
+    fuel_sum
+  end
+
+  def solve_part2() do
+    sum_of_fuel_fuel(get_input(), 0)
+  end
+
+  def sum_of_fuel_fuel([module | rest], fuel_sum) when module > 0 do
+    next_fuel =
+      case floor(module / 3) - 2 do
+        n when n > 0 -> n
+        _ -> 0
+      end
+
+    sum_of_fuel_fuel([next_fuel | rest], fuel_sum + next_fuel)
+  end
+
+  def sum_of_fuel_fuel([module | rest], fuel_sum) when module <= 0 do
+    sum_of_fuel_fuel(rest, fuel_sum)
+  end
+
+  def sum_of_fuel_fuel([], fuel_sum) do
     fuel_sum
   end
 end
--- day1/elixir/test/day1_test.exs
+++ day1/elixir/test/day1_test.exs
@@ -3,6 +3,12 @@ defmodule Day1Test do
 
   test "Day1.solve returns sum of fuel" do
     input = [12, 14, 1969, 100_756]
-    assert(Day1.solve(input) == 34241)
+    assert(Day1.sum_of_fuel(input, 0) == 34241)
+  end
+
+  test "Day1.solve2 returns sum of fuel" do
+    assert(Day1.sum_of_fuel_fuel([1969], 0) == 966)
+    assert(Day1.sum_of_fuel_fuel([100_756], 0) == 50346)
+    assert(Day1.sum_of_fuel_fuel([1969, 100_756], 0) == 51312)
   end
 end
