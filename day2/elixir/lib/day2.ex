c25d1c619c74d05219326146b86e0a2f327b779b
Warut Surapat <wsurapat@gilt.jp>
2019-12-02 15:22:05 +0900

    Start day 2.

---
+++ day2/elixir/lib/day2.ex
@@ -0,0 +1,18 @@
+defmodule Day2 do
+  @moduledoc """
+  Documentation for Day2.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Day2.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ day2/elixir/test/day2_test.exs
@@ -0,0 +1,8 @@
+defmodule Day2Test do
+  use ExUnit.Case
+  doctest Day2
+
+  test "greets the world" do
+    assert Day2.hello() == :world
+  end
+end
