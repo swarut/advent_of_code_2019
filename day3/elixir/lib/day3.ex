11e3a9eb7702eb06e0be56a5e9a6af644982afe9
Warut Surapat <wsurapat@gilt.jp>
2019-12-03 11:47:29 +0900

    Prepare for day 3.

---
+++ day3/elixir/lib/day3.ex
@@ -0,0 +1,18 @@
+defmodule Day3 do
+  @moduledoc """
+  Documentation for Day3.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Day3.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ day3/elixir/test/day3_test.exs
@@ -0,0 +1,8 @@
+defmodule Day3Test do
+  use ExUnit.Case
+  doctest Day3
+
+  test "greets the world" do
+    assert Day3.hello() == :world
+  end
+end
