8cbb02f85476d668caa3d3313602a4624632710d
Warut Surapat <s.warut@gmail.com>
2019-12-04 03:15:56 +0900

    Add day 3 part 2 solution.

--- day3/elixir/lib/day3.ex
+++ day3/elixir/lib/day3.ex
@@ -8,22 +8,53 @@ defmodule Day3 do
 
   def solve_part1() do
     [input1, input2] = get_input()
-    solve(input1, input2, fn {[x, y], _v} -> abs(x) + abs(y) end)
+    coordinates1 = expand_coordinates(input1, [0, 0], []) |> Enum.uniq()
+    coordinates2 = expand_coordinates(input2, [0, 0], []) |> Enum.uniq()
+    solve(coordinates1, coordinates2, fn {[x, y], _v} -> abs(x) + abs(y) end)
   end
 
-  def solve(input1, input2, distance) do
-    crossings = find_crossing(input1, input2)
+  def solve_part2() do
+    [input1, input2] = get_input()
+    coordinates1 = expand_coordinates(input1, [0, 0], [])
+    coordinates2 = expand_coordinates(input2, [0, 0], [])
+
+    solve(coordinates1, coordinates2, fn {[x, y], _v} ->
+      track_wire_distance(coordinates1, x, y) + track_wire_distance(coordinates2, x, y)
+    end)
+  end
+
+  def track_wire_distance(coordinates, x, y) do
+    wire_distance(coordinates, [x, y], 0)
+  end
+
+  def wire_distance([[x1, y1] | _rest], [target_x, target_y], acc)
+      when x1 == target_x and y1 == target_y do
+    acc + 1
+  end
+
+  def wire_distance([[_x1, _y1] | rest], target, acc) do
+    wire_distance(rest, target, acc + 1)
+  end
+
+  # Based on this implementation for part2, as there is no duplicate removal for each coordinate set,
+  # there can be a chance that the crossing point is not valid (for instance, same point is repeat twice
+  # just only by the same wire). A quick hack is to cheat by increasing the wire_distance to large number
+  # when we can't really find the target coordinate in a given wire.
+  def wire_distance([], _target, _acc) do
+    1_000_000
+  end
+
+  def solve(coordinates1, coordinates2, distance) do
+    crossings = find_crossing(coordinates1, coordinates2)
     distances = crossings |> Enum.map(distance) |> Enum.sort()
+    IO.puts("CROSSINGS = #{inspect(crossings)}")
+    IO.puts("DISTANCE = #{inspect(distances)}")
     [answer | _] = distances
     answer
   end
 
-  def find_crossing(input1, input2) do
-    coordinates1 = expand_coordinates(input1, [0, 0], []) |> Enum.uniq()
-    coordinates2 = expand_coordinates(input2, [0, 0], []) |> Enum.uniq()
-
-    plot(coordinates1 ++ coordinates2, %{})
-    |> Enum.filter(fn {_k, v} -> v == 2 end)
+  def find_crossing(coordinates1, coordinates2) do
+    plot(coordinates1 ++ coordinates2, %{}) |> Enum.filter(fn {_k, v} -> v >= 2 end)
   end
 
   def expand_coordinates(["U" <> count | rest], [x, y], acc) do
--- day3/elixir/test/day3_test.exs
+++ day3/elixir/test/day3_test.exs
@@ -8,21 +8,84 @@ defmodule Day3Test do
     assert coordinates == [[0, 0], [1, 0], [2, 0]]
   end
 
-  test "sample1" do
+  test "wire_distance" do
+    input1 = ["R8", "U5", "L5", "D3"]
+    start_point = [0, 0]
+    coordinates1 = Day3.expand_coordinates(input1, start_point, []) |> Enum.uniq()
+    assert(Day3.wire_distance(coordinates1, [8, 0], 0) == 8)
+    assert(Day3.wire_distance(coordinates1, [8, 5], 0) == 13)
+    assert(Day3.wire_distance(coordinates1, [3, 5], 0) == 18)
+  end
+
+  test "part1 sample1" do
     input1 = ["R8", "U5", "L5", "D3"]
     input2 = ["U7", "R6", "D4", "L4"]
-    assert(Day3.solve(input1, input2, fn {[x, y], _v} -> abs(x) + abs(y) end) == 6)
+    start_point = [0, 0]
+    coordinates1 = Day3.expand_coordinates(input1, start_point, []) |> Enum.uniq()
+    coordinates2 = Day3.expand_coordinates(input2, start_point, []) |> Enum.uniq()
+    assert(Day3.solve(coordinates1, coordinates2, fn {[x, y], _v} -> abs(x) + abs(y) end) == 6)
   end
 
-  test "sample2" do
+  test "part1 sample2" do
     input1 = ["R75", "D30", "R83", "U83", "L12", "D49", "R71", "U7", "L72"]
     input2 = ["U62", "R66", "U55", "R34", "D71", "R55", "D58", "R83"]
-    assert(Day3.solve(input1, input2, fn {[x, y], _v} -> abs(x) + abs(y) end) == 159)
+    start_point = [0, 0]
+    coordinates1 = Day3.expand_coordinates(input1, start_point, []) |> Enum.uniq()
+    coordinates2 = Day3.expand_coordinates(input2, start_point, []) |> Enum.uniq()
+    assert(Day3.solve(coordinates1, coordinates2, fn {[x, y], _v} -> abs(x) + abs(y) end) == 159)
   end
 
-  test "sample3" do
+  test "part1 sample3" do
     input1 = ["R98", "U47", "R26", "D63", "R33", "U87", "L62", "D20", "R33", "U53", "R51"]
     input2 = ["U98", "R91", "D20", "R16", "D67", "R40", "U7", "R15", "U6", "R7"]
-    assert(Day3.solve(input1, input2, fn {[x, y], _v} -> abs(x) + abs(y) end) == 135)
+    start_point = [0, 0]
+    coordinates1 = Day3.expand_coordinates(input1, start_point, []) |> Enum.uniq()
+    coordinates2 = Day3.expand_coordinates(input2, start_point, []) |> Enum.uniq()
+    assert(Day3.solve(coordinates1, coordinates2, fn {[x, y], _v} -> abs(x) + abs(y) end) == 135)
+  end
+
+  test "part2 sample1" do
+    input1 = ["R8", "U5", "L5", "D3"]
+    input2 = ["U7", "R6", "D4", "L4"]
+    start_point = [0, 0]
+    coordinates1 = Day3.expand_coordinates(input1, start_point, [])
+    coordinates2 = Day3.expand_coordinates(input2, start_point, [])
+
+    assert(
+      Day3.solve(coordinates1, coordinates2, fn {[x, y], _v} ->
+        Day3.track_wire_distance(coordinates1, x, y) +
+          Day3.track_wire_distance(coordinates2, x, y)
+      end) == 30
+    )
+  end
+
+  test "part2 sample2" do
+    input1 = ["R75", "D30", "R83", "U83", "L12", "D49", "R71", "U7", "L72"]
+    input2 = ["U62", "R66", "U55", "R34", "D71", "R55", "D58", "R83"]
+    start_point = [0, 0]
+    coordinates1 = Day3.expand_coordinates(input1, start_point, [])
+    coordinates2 = Day3.expand_coordinates(input2, start_point, [])
+
+    assert(
+      Day3.solve(coordinates1, coordinates2, fn {[x, y], _v} ->
+        Day3.track_wire_distance(coordinates1, x, y) +
+          Day3.track_wire_distance(coordinates2, x, y)
+      end) == 610
+    )
+  end
+
+  test "part2 sample3" do
+    input1 = ["R98", "U47", "R26", "D63", "R33", "U87", "L62", "D20", "R33", "U53", "R51"]
+    input2 = ["U98", "R91", "D20", "R16", "D67", "R40", "U7", "R15", "U6", "R7"]
+    start_point = [0, 0]
+    coordinates1 = Day3.expand_coordinates(input1, start_point, [])
+    coordinates2 = Day3.expand_coordinates(input2, start_point, [])
+
+    assert(
+      Day3.solve(coordinates1, coordinates2, fn {[x, y], _v} ->
+        Day3.track_wire_distance(coordinates1, x, y) +
+          Day3.track_wire_distance(coordinates2, x, y)
+      end) == 410
+    )
   end
 end
