b5bd8493f35a1a76574d6eeae5235cd0b86525c9
Warut Surapat <s.warut@gmail.com>
2019-12-04 01:33:34 +0900

    Add day 3 part 1 solution.

--- day3/elixir/lib/day3.ex
+++ day3/elixir/lib/day3.ex
@@ -2,19 +2,68 @@ defmodule Day3 do
   def get_input do
     {:ok, result} = File.read("input.txt")
 
-    String.split(result, [",", "\n"], trim: true)
-    |> Enum.map(fn x -> String.to_integer(x) end)
+    String.split(result, ["\n"], trim: true)
+    |> Enum.map(fn line -> line |> String.split([","], trim: true) end)
   end
 
   def solve_part1() do
+    [input1, input2] = get_input()
+    s(input1, input2)
+  end
 
+  def s(input1, input2) do
+    crossings = find_crossing(input1, input2)
+    distances = crossings |> Enum.map(fn {[x, y], _v} -> abs(x) + abs(y) end) |> Enum.sort()
+    [answer | _] = distances
+    answer
   end
 
   def find_crossing(input1, input2) do
-    [input1, input2]
+    coordinates1 = expand_coordinates(input1, [0, 0], []) |> Enum.uniq()
+    coordinates2 = expand_coordinates(input2, [0, 0], []) |> Enum.uniq()
+
+    plot(coordinates1 ++ coordinates2, %{})
+    |> Enum.filter(fn {_k, v} -> v == 2 end)
   end
 
-  def expand_coordinates([x, y] = _origin, input) do
+  def expand_coordinates(["U" <> count | rest], [x, y], acc) do
+    count = String.to_integer(count)
+    acc = acc ++ (1..count |> Enum.map(fn p -> [x, y + p] end))
+    new_start = [x, y + count]
+    expand_coordinates(rest, new_start, acc)
+  end
 
+  def expand_coordinates(["D" <> count | rest], [x, y], acc) do
+    count = String.to_integer(count)
+    acc = acc ++ (1..count |> Enum.map(fn p -> [x, y - p] end))
+    new_start = [x, y - count]
+    expand_coordinates(rest, new_start, acc)
+  end
+
+  def expand_coordinates(["R" <> count | rest], [x, y], acc) do
+    count = String.to_integer(count)
+    acc = acc ++ (1..count |> Enum.map(fn p -> [x + p, y] end))
+    new_start = [x + count, y]
+    expand_coordinates(rest, new_start, acc)
+  end
+
+  def expand_coordinates(["L" <> count | rest], [x, y], acc) do
+    count = String.to_integer(count)
+    acc = acc ++ (1..count |> Enum.map(fn p -> [x - p, y] end))
+    new_start = [x - count, y]
+    expand_coordinates(rest, new_start, acc)
+  end
+
+  def expand_coordinates([], [_x, _y], acc) do
+    acc
+  end
+
+  def plot([coordinate | rest], acc) do
+    acc = acc |> Map.update(coordinate, 1, fn x -> x + 1 end)
+    plot(rest, acc)
+  end
+
+  def plot([], acc) do
+    acc
   end
 end
--- day3/elixir/test/day3_test.exs
+++ day3/elixir/test/day3_test.exs
@@ -1,17 +1,28 @@
 defmodule Day3Test do
   use ExUnit.Case
 
-  # test "find crossing" do
-  #   input1 = ["R8", "U5", "L5", "D3"]
-  #   input2 = ["U7", "R6", "D4", "L4"]
-  #   crossings = Day3.find_crossings(input1, input2)
-
-  #   assert crossings == [[3, 3], [5, 6]]
-  # end
-
-  test "expand coordinates" do
+  test "expands coordinates" do
     input = ["R2"]
-    coordinates = Day3.expand_coordinates([0, 0], input)
-    assert coordinates == [[0, 0], [0, 1], [0, 2]]
+    start_point = [0, 0]
+    coordinates = Day3.expand_coordinates(input, start_point, [start_point])
+    assert coordinates == [[0, 0], [1, 0], [2, 0]]
+  end
+
+  test "sample1" do
+    input1 = ["R8", "U5", "L5", "D3"]
+    input2 = ["U7", "R6", "D4", "L4"]
+    assert(Day3.s(input1, input2) == 6)
+  end
+
+  test "sample2" do
+    input1 = ["R75", "D30", "R83", "U83", "L12", "D49", "R71", "U7", "L72"]
+    input2 = ["U62", "R66", "U55", "R34", "D71", "R55", "D58", "R83"]
+    assert(Day3.s(input1, input2) == 159)
+  end
+
+  test "sample3" do
+    input1 = ["R98", "U47", "R26", "D63", "R33", "U87", "L62", "D20", "R33", "U53", "R51"]
+    input2 = ["U98", "R91", "D20", "R16", "D67", "R40", "U7", "R15", "U6", "R7"]
+    assert(Day3.s(input1, input2) == 135)
   end
 end
