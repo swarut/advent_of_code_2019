58787adc8950c685df095e32253862ac7843866b
Warut Surapat <s.warut@gmail.com>
2019-12-05 00:03:02 +0900

    Start day4.

---
+++ day4/elixir/lib/day4.ex
@@ -0,0 +1,18 @@
+defmodule Day4 do
+  @moduledoc """
+  Documentation for Day4.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Day4.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ day4/elixir/test/day4_test.exs
@@ -0,0 +1,8 @@
+defmodule Day4Test do
+  use ExUnit.Case
+  doctest Day4
+
+  test "greets the world" do
+    assert Day4.hello() == :world
+  end
+end
