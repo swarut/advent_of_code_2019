fc7a4f5c1396eead213f7ad6917e29a258219109
Warut Surapat <s.warut@gmail.com>
2019-12-05 01:30:31 +0900

    Solve day 4 part 2.

--- day4/elixir/lib/day4.ex
+++ day4/elixir/lib/day4.ex
@@ -12,6 +12,19 @@ defmodule Day4 do
     end)
   end
 
+  def solve_part2() do
+    136_818..685_979
+    |> Enum.reduce(0, fn x, acc ->
+      case accept2(x) do
+        true ->
+          acc + 1
+
+        false ->
+          acc
+      end
+    end)
+  end
+
   def accept(num) do
     accept(num, %{last: nil, two_adj: false, break_order: false})
   end
@@ -30,5 +43,45 @@ defmodule Day4 do
 
   def accept(0, %{last: _last, two_adj: two_adj, break_order: break_order}) do
     two_adj && !break_order
+  end
+
+  def accept2(num) do
+    accept2(num, %{last: nil, two_adj: false, adjs: [], break_order: false})
+  end
+
+  def accept2(_num, %{last: _last, two_adj: _two_adj, adjs: _adjs, break_order: true}) do
+    false
+  end
+
+  def accept2(num, %{
+        last: last,
+        two_adj: two_adj,
+        adjs: adjs,
+        break_order: _break_order
+      })
+      when num > 0 do
+    dec = rem(num, 10)
+    next_num = div(num, 10)
+    is_two_adj = two_adj || dec == last
+
+    adjs =
+      case is_two_adj && dec == last do
+        true -> [dec | adjs]
+        false -> adjs
+      end
+
+    break_order = last && dec > last
+    accept2(next_num, %{last: dec, two_adj: is_two_adj, adjs: adjs, break_order: break_order})
+  end
+
+  def accept2(0, %{last: _last, two_adj: two_adj, adjs: adjs, break_order: break_order}) do
+    has_exactly_two_adjacent =
+      adjs
+      |> Enum.reduce(%{}, fn x, acc ->
+        acc |> Map.update(x, 1, fn y -> y + 1 end)
+      end)
+      |> Enum.find_value(fn {_k, v} -> v == 1 end)
+
+    two_adj && !break_order && has_exactly_two_adjacent != nil
   end
 end
--- day4/elixir/test/day4_test.exs
+++ day4/elixir/test/day4_test.exs
@@ -8,4 +8,13 @@ defmodule Day4Test do
     assert(Day4.accept(122) == true)
     assert(Day4.accept(221) == false)
   end
+
+  test "accept2" do
+    assert(Day4.accept2(11) == true)
+    assert(Day4.accept2(12) == false)
+    assert(Day4.accept2(1223) == true)
+    assert(Day4.accept2(1221) == false)
+    assert(Day4.accept2(1222) == false)
+    assert(Day4.accept2(122_244) == true)
+  end
 end
