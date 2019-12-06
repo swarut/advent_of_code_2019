c3dba2aff061abff1e82329d5a7c76c1167c796a
Warut Surapat <s.warut@gmail.com>
2019-12-06 23:20:34 +0900

    Solve day 5 part 1.

---
+++ day5/elixir/lib/day5.ex
@@ -0,0 +1,95 @@
+defmodule Day5 do
+  def get_input do
+    {:ok, result} = File.read("input.txt")
+
+    String.split(result, [",", "\n"], trim: true)
+    |> Enum.map(fn x -> String.to_integer(x) end)
+  end
+
+  def solve_part1()  do
+    input = get_input()
+    process(preprocess_input(input), input, 0, [])
+  end
+
+  # Preprocess input by converting the first item in the list from integer to list of digit.
+  def preprocess_input(input) do
+    [head | rest] = input
+    instruction = normalize_instruction(head)
+    [instruction | rest]
+  end
+
+  def normalize_instruction(n) do
+    [100, 10, 10, 10]
+    |> Enum.reduce(%{num: n, list: []}, fn i, acc ->
+      acc
+      |> Map.put(:list, [rem(acc[:num], i) | acc[:list]])
+      |> Map.put(:num, div(acc[:num], i))
+    end)
+    |> Map.get(:list)
+  end
+
+  # Position mode
+  def get_value(0, input, kv) do
+    input |> Enum.at(kv)
+  end
+
+  # Immediate mode
+  def get_value(1, _input, kv) do
+    kv
+  end
+
+  def process([[_m3, m2, m1, 1], p1, p2, p3 | _rest], memory, last_cursor, acc) do
+    s = get_value(m1, memory, p1) + get_value(m2, memory, p2)
+    memory = memory |> List.replace_at(p3, s)
+    last_cursor = last_cursor + 4
+
+    process(
+      preprocess_input(memory |> Enum.drop(last_cursor)),
+      memory,
+      last_cursor,
+      acc
+    )
+  end
+
+  def process([[_m3, m2, m1, 2], p1, p2, p3 | _rest], memory, last_cursor, acc) do
+    s = get_value(m1, memory, p1) * get_value(m2, memory, p2)
+    memory = memory |> List.replace_at(p3, s)
+    last_cursor = last_cursor + 4
+
+    process(
+      preprocess_input(memory |> Enum.drop(last_cursor)),
+      memory,
+      last_cursor,
+      acc
+    )
+  end
+
+  def process([[_m3, _m2, _m1, 3], p1 | _rest], memory, last_cursor, acc) do
+    val = IO.gets("Please input a number:") |> String.trim() |> String.to_integer()
+    memory = memory |> List.replace_at(p1, val)
+    last_cursor = last_cursor + 2
+
+    process(
+      preprocess_input(memory |> Enum.drop(last_cursor)),
+      memory,
+      last_cursor,
+      acc
+    )
+  end
+
+  def process([[_m3, _m2, _m1, 4], p1 | _rest], memory, last_cursor, acc) do
+    acc = [memory |> Enum.at(p1) | acc]
+    last_cursor = last_cursor + 2
+
+    process(
+      preprocess_input(memory |> Enum.drop(last_cursor)),
+      memory,
+      last_cursor,
+      acc
+    )
+  end
+
+  def process([[_m3, _m2, _m1, 99] | _rest], memory, _last_cursor, acc) do
+    [acc, memory]
+  end
+end
