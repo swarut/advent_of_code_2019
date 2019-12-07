12737eecee68c154cf318e4088d3662dc13c477d
Warut Surapat <s.warut@gmail.com>
2019-12-07 16:59:20 +0900

    Start day 6.

---
+++ day6/elixir/lib/day6.ex
@@ -0,0 +1,18 @@
+defmodule Day6 do
+  @moduledoc """
+  Documentation for Day6.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Day6.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ day6/elixir/test/day6_test.exs
@@ -0,0 +1,8 @@
+defmodule Day6Test do
+  use ExUnit.Case
+  doctest Day6
+
+  test "greets the world" do
+    assert Day6.hello() == :world
+  end
+end
