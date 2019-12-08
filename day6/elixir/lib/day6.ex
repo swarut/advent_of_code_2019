ec9a4665f25de16facb19487892e787d34c6d8a2
Warut Surapat <s.warut@gmail.com>
2019-12-08 09:46:44 +0900

    Solve day 6 part 1.

--- day6/elixir/lib/day6.ex
+++ day6/elixir/lib/day6.ex
@@ -1,18 +1,62 @@
 defmodule Day6 do
-  @moduledoc """
-  Documentation for Day6.
-  """
+  def get_input do
+    {:ok, result} = File.read("input.txt")
 
-  @doc """
-  Hello world.
+    String.split(result, [",", "\n"], trim: true)
+  end
 
-  ## Examples
+  def solve_part1() do
+    children_lookup = preprocess_input(get_input())
+    solve(children_lookup)
+  end
 
-      iex> Day6.hello()
-      :world
+  def preprocess_input(input) do
+    input
+    |> Enum.reduce(%{}, fn line, acc ->
+      [parent, child] = parse(line)
 
-  """
-  def hello do
-    :world
+      acc =
+        case acc[parent] do
+          nil ->
+            acc |> Map.put(parent, %{parent: nil, all_parents_count: nil})
+
+          _ ->
+            acc
+        end
+
+      acc |> Map.put(child, %{parent: parent, all_parents_count: nil})
+    end)
+  end
+
+  def solve(children_lookup) do
+    children = children_lookup |> Map.keys()
+
+    children
+    |> Enum.reduce(%{children_lookup: children_lookup, count: 0}, fn child, acc ->
+      child_data = acc[:children_lookup][child]
+      parent_count = count_parent(child_data, acc[:children_lookup], 0)
+      child_data = child_data |> Map.put(:all_parents_count, parent_count)
+      children_lookup = acc[:children_lookup] |> Map.put(child, child_data)
+
+      acc
+      |> Map.put(:count, acc[:count] + parent_count)
+      |> Map.put(:children_lookup, children_lookup)
+    end)
+  end
+
+  def parse(<<n1::binary-size(3)>> <> ")" <> <<n2::binary-size(3)>>) do
+    [n1, n2]
+  end
+
+  def count_parent(%{parent: nil, all_parents_count: _all_parents_count}, _children_lookup, acc) do
+    acc
+  end
+
+  def count_parent(%{parent: parent, all_parents_count: nil}, children_lookup, acc) do
+    count_parent(children_lookup[parent], children_lookup, acc + 1)
+  end
+
+  def count_parent(%{parent: _parent, all_parents_count: count}, _children_lookup, acc) do
+    count + acc
   end
 end
--- day6/elixir/test/day6_test.exs
+++ day6/elixir/test/day6_test.exs
@@ -1,8 +1,79 @@
 defmodule Day6Test do
   use ExUnit.Case
-  doctest Day6
 
-  test "greets the world" do
-    assert Day6.hello() == :world
+  test "parse" do
+    assert(Day6.parse("aaa)bbb") == ["aaa", "bbb"])
+  end
+
+  test "preprocess input" do
+    input = ["aaa)bbb", "bbb)ccc", "ccc)ddd", "eee)fff"]
+
+    assert(
+      Day6.preprocess_input(input) == %{
+        "aaa" => %{parent: nil, all_parents_count: nil},
+        "bbb" => %{parent: "aaa", all_parents_count: nil},
+        "ccc" => %{parent: "bbb", all_parents_count: nil},
+        "ddd" => %{parent: "ccc", all_parents_count: nil},
+        "eee" => %{parent: nil, all_parents_count: nil},
+        "fff" => %{parent: "eee", all_parents_count: nil}
+      }
+    )
+  end
+
+  test "solve" do
+    input = ["aaa)bbb", "bbb)ccc", "ccc)ddd", "eee)fff"]
+    children_lookup = Day6.preprocess_input(input)
+
+    assert(
+      Day6.solve(children_lookup) == %{
+        children_lookup: %{
+          "aaa" => %{parent: nil, all_parents_count: 0},
+          "bbb" => %{parent: "aaa", all_parents_count: 1},
+          "ccc" => %{parent: "bbb", all_parents_count: 2},
+          "ddd" => %{parent: "ccc", all_parents_count: 3},
+          "eee" => %{parent: nil, all_parents_count: 0},
+          "fff" => %{parent: "eee", all_parents_count: 1}
+        },
+        count: 7
+      }
+    )
+  end
+
+  test "solve2" do
+    input = [
+      "COM)BBB",
+      "BBB)CCC",
+      "CCC)DDD",
+      "DDD)EEE",
+      "EEE)FFF",
+      "BBB)GGG",
+      "GGG)HHH",
+      "DDD)III",
+      "EEE)JJJ",
+      "JJJ)KKK",
+      "KKK)LLL"
+    ]
+
+    children_lookup = Day6.preprocess_input(input)
+
+    assert(
+      Day6.solve(children_lookup) == %{
+        children_lookup: %{
+          "COM" => %{parent: nil, all_parents_count: 0},
+          "BBB" => %{parent: "COM", all_parents_count: 1},
+          "CCC" => %{parent: "BBB", all_parents_count: 2},
+          "DDD" => %{parent: "CCC", all_parents_count: 3},
+          "EEE" => %{parent: "DDD", all_parents_count: 4},
+          "FFF" => %{parent: "EEE", all_parents_count: 5},
+          "GGG" => %{parent: "BBB", all_parents_count: 2},
+          "HHH" => %{parent: "GGG", all_parents_count: 3},
+          "III" => %{parent: "DDD", all_parents_count: 4},
+          "JJJ" => %{parent: "EEE", all_parents_count: 5},
+          "KKK" => %{parent: "JJJ", all_parents_count: 6},
+          "LLL" => %{parent: "KKK", all_parents_count: 7}
+        },
+        count: 42
+      }
+    )
   end
 end
