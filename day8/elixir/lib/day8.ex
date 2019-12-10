852ed7df1ed462eacbfd96ea6878c890da4bbad3
Warut Surapat <wsurapat@gilt.jp>
2019-12-10 17:02:35 +0900

    Start day 8.

---
+++ day8/elixir/lib/day8.ex
@@ -0,0 +1,10 @@
+defmodule Day8 do
+  def get_input do
+    {:ok, result} = File.read("input.txt")
+    result
+  end
+
+  def solve_part1() do
+    get_input()
+  end
+end
---
+++ day8/elixir/test/day8_test.exs
@@ -0,0 +1,8 @@
+defmodule Day8Test do
+  use ExUnit.Case
+  doctest Day8
+
+  test "greets the world" do
+    assert Day8.hello() == :world
+  end
+end
