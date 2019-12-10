b1449adfbee75f832ba52789786ddce1aec33127
Warut Surapat <wsurapat@gilt.jp>
2019-12-10 19:50:54 +0900

    Solve day 8 both parts.

--- day8/elixir/lib/day8.ex
+++ day8/elixir/lib/day8.ex
@@ -1,10 +1,63 @@
 defmodule Day8 do
   def get_input do
     {:ok, result} = File.read("input.txt")
-    result
+    result |> String.trim() |> String.graphemes()
   end
 
-  def solve_part1() do
-    get_input()
+  def solve_part1(width, height) do
+    input = get_input()
+
+    layers = input |> Enum.chunk_every(width * height)
+
+    counts =
+      layers
+      |> Enum.map(fn layer ->
+        IO.puts("Layer : #{layer}")
+        do_count(layer, %{})
+      end)
+      |> Enum.min_by(fn x -> x["0"] end)
+
+    IO.puts("Result = #{inspect(counts)}")
+    merged_layer = perform_merge(layers)
+    lines = merged_layer |> Enum.chunk_every(width)
+
+    lines
+    |> Enum.each(fn x ->
+      IO.puts(x |> Enum.join())
+    end)
+  end
+
+  def do_count([h | t], acc) do
+    acc = acc |> Map.update(h, 1, fn x -> x + 1 end)
+    do_count(t, acc)
+  end
+
+  def do_count([], acc) do
+    acc
+  end
+
+  def perform_merge([layer1, layer2 | rest]) do
+    merged = merge_layers(layer1, layer2, [])
+    perform_merge([merged | rest])
+  end
+
+  def perform_merge([last_layer]) do
+    last_layer
+  end
+
+  def merge_layers([l1 | rest1], [l2 | rest2], acc) do
+    acc = [merge(l1, l2) | acc]
+    merge_layers(rest1, rest2, acc)
+  end
+
+  def merge_layers([], [], acc) do
+    acc |> Enum.reverse()
+  end
+
+  def merge(l1, l2) do
+    case l1 do
+      "2" -> l2
+      _ -> l1
+    end
   end
 end
--- day8/elixir/test/day8_test.exs
+++ day8/elixir/test/day8_test.exs
@@ -1,8 +1,24 @@
 defmodule Day8Test do
   use ExUnit.Case
-  doctest Day8
 
-  test "greets the world" do
-    assert Day8.hello() == :world
+  test "merge layers" do
+    layer1 = ["2", "2", "1", "0", "2", "2"]
+    layer2 = ["1", "0", "0", "1", "1", "2"]
+    assert(Day8.merge_layers(layer1, layer2, []) == ["1", "0", "1", "0", "1", "2"])
+  end
+
+  test "perform merge" do
+    layer1 = ["2", "2", "1", "0", "2", "2"]
+    layer2 = ["1", "0", "0", "1", "1", "2"]
+    layer3 = ["2", "0", "2", "1", "1", "1"]
+    assert(Day8.perform_merge([layer1, layer2, layer3]) == ["1", "0", "1", "0", "1", "1"])
+  end
+
+  test "perform merge 2" do
+    layer1 = ["0", "2", "2", "2"]
+    layer2 = ["1", "1", "2", "2"]
+    layer3 = ["2", "2", "1", "2"]
+    layer4 = ["0", "0", "0", "0"]
+    assert(Day8.perform_merge([layer1, layer2, layer3, layer4]) == ["0", "1", "1", "0"])
   end
 end
