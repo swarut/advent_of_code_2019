107bc31935254e309fed386e0f29e4f711f1dffc
Warut Surapat <s.warut@gmail.com>
2019-12-12 02:56:45 +0900

    Implement all day 9 part 1 requiremt, but the answer is incorrect.

--- day9/elixir/lib/day9.ex
+++ day9/elixir/lib/day9.ex
@@ -6,9 +6,12 @@ defmodule Day9 do
     |> Enum.map(fn x -> String.to_integer(x) end)
   end
 
-  def solve_part1()  do
+  def solve_part1() do
     instructions = get_instructions()
-    [[result | _rest], _] = process(preprocess_instruction(instructions), instructions, 0, %{offset: 0}, [])
+
+    [[result | _rest], _] =
+      process(preprocess_instruction(instructions), instructions, 0, %{offset: 0}, [])
+
     IO.puts("Result = #{result}")
     result
   end
@@ -33,20 +36,49 @@ defmodule Day9 do
 
   # Position mode
   def get_value(0, memory, kv, _options) do
-    {:ok, memory |> Enum.at(kv)}
+    value = memory |> Enum.at(kv)
+
+    case value do
+      nil -> {:error, memory, 0, kv}
+      _ -> {:ok, memory, value}
+    end
   end
 
   # Immediate mode
-  def get_value(1, _memory, kv, _options) do
-    {:ok, kv}
+  def get_value(1, memory, kv, _options) do
+    {:ok, memory, kv}
   end
 
   # Relative mode
   def get_value(2, memory, kv, %{offset: offset}) do
-    {:ok, memory |> Enum.at(offset + kv)}
+    value = memory |> Enum.at(offset + kv)
+
+    case value do
+      nil -> {:error, memory, 0, offset + kv}
+      _ -> {:ok, memory, value}
+    end
   end
 
-  def process([[_m3, _m2, _m1, 9], p1 | _rest], memory, last_cursor, %{offset: offset} = options, acc) do
+  def expand_memory_if_needed({:ok, memory, value}) do
+    {value, memory}
+  end
+
+  def expand_memory_if_needed({:error, memory, value, position}) do
+    space_needed = position - length(memory) + 1
+    IO.puts("Memory access at #{position}, space_needed = #{space_needed}")
+    allocated = List.duplicate(0, space_needed)
+    memory = memory ++ allocated
+    IO.puts("UPDATED MEMORY = #{inspect(memory)}")
+    {value, memory ++ allocated}
+  end
+
+  def process(
+        [[_m3, _m2, _m1, 9], p1 | _rest],
+        memory,
+        last_cursor,
+        %{offset: offset} = options,
+        acc
+      ) do
     options = options |> Map.put(:offset, offset + p1)
     last_cursor = last_cursor + 2
 
@@ -60,8 +92,8 @@ defmodule Day9 do
   end
 
   def process([[_m3, m2, m1, 1], p1, p2, p3 | _rest], memory, last_cursor, options, acc) do
-    {:ok, s1} = get_value(m1, memory, p1, options)
-    {:ok, s2} = get_value(m2, memory, p2, options)
+    {s1, memory} = get_value(m1, memory, p1, options) |> expand_memory_if_needed()
+    {s2, memory} = get_value(m2, memory, p2, options) |> expand_memory_if_needed()
     s = s1 + s2
     memory = memory |> List.replace_at(p3, s)
     last_cursor = last_cursor + 4
@@ -76,8 +108,8 @@ defmodule Day9 do
   end
 
   def process([[_m3, m2, m1, 2], p1, p2, p3 | _rest], memory, last_cursor, options, acc) do
-    {:ok, s1} = get_value(m1, memory, p1, options)
-    {:ok, s2} = get_value(m2, memory, p2, options)
+    {s1, memory} = get_value(m1, memory, p1, options) |> expand_memory_if_needed()
+    {s2, memory} = get_value(m2, memory, p2, options) |> expand_memory_if_needed()
     s = s1 * s2
     memory = memory |> List.replace_at(p3, s)
     last_cursor = last_cursor + 4
@@ -92,13 +124,17 @@ defmodule Day9 do
   end
 
   def process([[_m3, m2, m1, 5], p1, p2 | _rest], memory, last_cursor, options, acc) do
-    {:ok, s} = get_value(m1, memory, p1, options)
-    last_cursor = case s do
-      0 -> last_cursor + 3
-      _ ->
-        {:ok, l} = get_value(m2, memory, p2, options)
-        l
-    end
+    {s, memory} = get_value(m1, memory, p1, options) |> expand_memory_if_needed()
+
+    {last_cursor, memory} =
+      case s do
+        0 ->
+          {last_cursor + 3, memory}
+
+        _ ->
+          get_value(m2, memory, p2, options) |> expand_memory_if_needed()
+      end
+
     process(
       preprocess_instruction(memory |> Enum.drop(last_cursor)),
       memory,
@@ -109,13 +145,16 @@ defmodule Day9 do
   end
 
   def process([[_m3, m2, m1, 6], p1, p2 | _rest], memory, last_cursor, options, acc) do
-    {:ok, s} = get_value(m1, memory, p1, options)
-    last_cursor = case s do
-      0 ->
-        {:ok, l} = get_value(m2, memory, p2, options)
-        l
-      _ -> last_cursor + 3
-    end
+    {s, memory} = get_value(m1, memory, p1, options) |> expand_memory_if_needed()
+
+    {last_cursor, memory} =
+      case s do
+        0 ->
+          get_value(m2, memory, p2, options) |> expand_memory_if_needed()
+
+        _ ->
+          {last_cursor + 3, memory}
+      end
 
     process(
       preprocess_instruction(memory |> Enum.drop(last_cursor)),
@@ -127,8 +166,8 @@ defmodule Day9 do
   end
 
   def process([[_m3, m2, m1, 7], p1, p2, p3 | _rest], memory, last_cursor, options, acc) do
-    {:ok, s1} = get_value(m1, memory, p1, options)
-    {:ok, s2} = get_value(m2, memory, p2, options)
+    {s1, memory} = get_value(m1, memory, p1, options) |> expand_memory_if_needed()
+    {s2, memory} = get_value(m2, memory, p2, options) |> expand_memory_if_needed()
 
     memory =
       cond do
@@ -148,8 +187,8 @@ defmodule Day9 do
   end
 
   def process([[_m3, m2, m1, 8], p1, p2, p3 | _rest], memory, last_cursor, options, acc) do
-    {:ok, s1} = get_value(m1, memory, p1, options)
-    {:ok, s2} = get_value(m2, memory, p2, options)
+    {s1, memory} = get_value(m1, memory, p1, options) |> expand_memory_if_needed()
+    {s2, memory} = get_value(m2, memory, p2, options) |> expand_memory_if_needed()
 
     memory =
       cond do
@@ -182,8 +221,9 @@ defmodule Day9 do
     )
   end
 
-  def process([[_m3, _m2, _m1, 4], p1 | _rest], memory, last_cursor, options, acc) do
-    acc = [memory |> Enum.at(p1) | acc]
+  def process([[_m3, _m2, m1, 4], p1 | _rest], memory, last_cursor, options, acc) do
+    {s1, memory} = get_value(m1, memory, p1, options) |> expand_memory_if_needed()
+    acc = [s1 | acc]
     last_cursor = last_cursor + 2
 
     process(
@@ -196,6 +236,6 @@ defmodule Day9 do
   end
 
   def process([[_m3, _m2, _m1, 99] | _rest], memory, _last_cursor, _options, acc) do
-    [acc, memory]
+    [acc |> Enum.reverse(), memory]
   end
 end
--- day9/elixir/test/day9_test.exs
+++ day9/elixir/test/day9_test.exs
@@ -1,53 +1,83 @@
 defmodule Day9Test do
   use ExUnit.Case
 
-  test "normalize instruction" do
-    assert(Day9.normalize_instruction(1) == [0, 0, 0, 1])
-    assert(Day9.normalize_instruction(2) == [0, 0, 0, 2])
-    assert(Day9.normalize_instruction(3) == [0, 0, 0, 3])
-    assert(Day9.normalize_instruction(4) == [0, 0, 0, 4])
-    assert(Day9.normalize_instruction(99) == [0, 0, 0, 99])
+  # test "normalize instruction" do
+  #   assert(Day9.normalize_instruction(1) == [0, 0, 0, 1])
+  #   assert(Day9.normalize_instruction(2) == [0, 0, 0, 2])
+  #   assert(Day9.normalize_instruction(3) == [0, 0, 0, 3])
+  #   assert(Day9.normalize_instruction(4) == [0, 0, 0, 4])
+  #   assert(Day9.normalize_instruction(99) == [0, 0, 0, 99])
 
-    assert(Day9.normalize_instruction(101) == [0, 0, 1, 1])
-    assert(Day9.normalize_instruction(1101) == [0, 1, 1, 1])
-    assert(Day9.normalize_instruction(11101) == [1, 1, 1, 1])
-    assert(Day9.normalize_instruction(10101) == [1, 0, 1, 1])
-    assert(Day9.normalize_instruction(11001) == [1, 1, 0, 1])
-    assert(Day9.normalize_instruction(10001) == [1, 0, 0, 1])
+  #   assert(Day9.normalize_instruction(101) == [0, 0, 1, 1])
+  #   assert(Day9.normalize_instruction(1101) == [0, 1, 1, 1])
+  #   assert(Day9.normalize_instruction(11101) == [1, 1, 1, 1])
+  #   assert(Day9.normalize_instruction(10101) == [1, 0, 1, 1])
+  #   assert(Day9.normalize_instruction(11001) == [1, 1, 0, 1])
+  #   assert(Day9.normalize_instruction(10001) == [1, 0, 0, 1])
 
-    assert(Day9.normalize_instruction(11099) == [1, 1, 0, 99])
-    assert(Day9.normalize_instruction(11199) == [1, 1, 1, 99])
-    assert(Day9.normalize_instruction(10199) == [1, 0, 1, 99])
-    assert(Day9.normalize_instruction(11099) == [1, 1, 0, 99])
-    assert(Day9.normalize_instruction(10099) == [1, 0, 0, 99])
-  end
+  #   assert(Day9.normalize_instruction(11099) == [1, 1, 0, 99])
+  #   assert(Day9.normalize_instruction(11199) == [1, 1, 1, 99])
+  #   assert(Day9.normalize_instruction(10199) == [1, 0, 1, 99])
+  #   assert(Day9.normalize_instruction(11099) == [1, 1, 0, 99])
+  #   assert(Day9.normalize_instruction(10099) == [1, 0, 0, 99])
+  # end
 
-  test "preprocess input" do
-    assert(Day9.preprocess_instruction([101, 1, 1]) == [[0, 0, 1, 1], 1, 1])
-    assert(Day9.preprocess_instruction([10099, 3, 4]) == [[1, 0, 0, 99], 3, 4])
-  end
+  # test "preprocess input" do
+  #   assert(Day9.preprocess_instruction([101, 1, 1]) == [[0, 0, 1, 1], 1, 1])
+  #   assert(Day9.preprocess_instruction([10099, 3, 4]) == [[1, 0, 0, 99], 3, 4])
+  # end
 
-  test "process" do
-    input = [1, 0, 0, 0, 99]
+  # test "process" do
+  #   input = [1, 0, 0, 0, 99]
+  #   pp = Day9.preprocess_instruction(input)
+  #   assert(Day9.process(pp, input, 0, %{offset: 0}, []) == [[], [2, 0, 0, 0, 99]])
+  # end
+
+  # test "process2" do
+  #   input = [2, 3, 0, 3, 99]
+  #   pp = Day9.preprocess_instruction(input)
+  #   assert(Day9.process(pp, input, 0, %{offset: 0}, []) == [[], [2, 3, 0, 6, 99]])
+  # end
+
+  # test "process3" do
+  #   input = [5, 10, 3, 4, 4, 2, 99]
+  #   pp = Day9.preprocess_instruction(input)
+
+  #   assert(
+  #     Day9.process(pp, input, 0, %{offset: 0}, []) == [[3], [5, 10, 3, 4, 4, 2, 99, 0, 0, 0, 0]]
+  #   )
+  # end
+
+  # test "process4" do
+  #   input = [9, 1, 201, 1, 2, 0, 4, 0, 99]
+  #   pp = Day9.preprocess_instruction(input)
+
+  #   assert(
+  #     Day9.process(pp, input, 0, %{offset: 0}, []) == [[402], [402, 1, 201, 1, 2, 0, 4, 0, 99]]
+  #   )
+  # end
+
+  # test "program that outputs itself" do
+  #   input = [109, 1, 204, -1, 1001, 100, 1, 100, 1008, 100, 16, 101, 1006, 101, 0, 99]
+  #   pp = Day9.preprocess_instruction(input)
+
+  #   [output, _] = Day9.process(pp, input, 0, %{offset: 0}, [])
+
+  #   assert(output == input)
+  # end
+
+  test "program that returns 16-digit number" do
+    input = [1102, 34_915_192, 34_915_192, 7, 4, 7, 99, 0]
     pp = Day9.preprocess_instruction(input)
-    assert(Day9.process(pp, input, 0, %{offset: 0}, []) == [[], [2, 0, 0, 0, 99]])
+    [[output], _] = Day9.process(pp, input, 0, %{offset: 0}, [])
+    output = output |> to_string() |> String.length()
+    assert(output == 16)
   end
 
-  test "process2" do
-    input = [2, 3, 0, 3, 99]
+  test "process7" do
+    input = [104, 1_125_899_906_842_624, 99]
     pp = Day9.preprocess_instruction(input)
-    assert(Day9.process(pp, input, 0, %{offset: 0}, []) == [[], [2, 3, 0, 6, 99]])
-  end
-
-  test "process3" do
-    input = [5, 10, 3, 4, 4, 2, 99]
-    pp = Day9.preprocess_instruction(input)
-    assert(Day9.process(pp, input, 0, %{offset: 0}, []) == [[3], [5, 10, 3, 4, 4, 2, 99]])
-  end
-
-  test "process4" do
-    input = [9, 1, 201, 1, 2, 0, 4, 0, 99]
-    pp = Day9.preprocess_instruction(input)
-    assert(Day9.process(pp, input, 0, %{offset: 0}, []) == [[402], [402, 1, 201, 1, 2, 0, 4, 0, 99]])
+    [[output], _] = Day9.process(pp, input, 0, %{offset: 0}, [])
+    assert(output == 1_125_899_906_842_624)
   end
 end
