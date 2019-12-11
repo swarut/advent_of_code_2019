2fee96f7a6e16010d7efc3f96b868b37e5deccd0
Warut Surapat <wsurapat@gilt.jp>
2019-12-11 15:00:24 +0900

    Start day 9.

---
+++ day9/elixir/lib/day9.ex
@@ -0,0 +1,18 @@
+defmodule Day9 do
+  @moduledoc """
+  Documentation for Day9.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Day9.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ day9/elixir/test/day9_test.exs
@@ -0,0 +1,8 @@
+defmodule Day9Test do
+  use ExUnit.Case
+  doctest Day9
+
+  test "greets the world" do
+    assert Day9.hello() == :world
+  end
+end
