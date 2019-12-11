9f8cc5095fdacf88e84af20c1e6a8d6bf7b12b28
Warut Surapat <wsurapat@gilt.jp>
2019-12-11 15:39:05 +0900

    Fix day5 test.

--- day5/elixir/test/day5_test.exs
+++ day5/elixir/test/day5_test.exs
@@ -1,48 +1,48 @@
 defmodule Day5Test do
   use ExUnit.Case
 
-  # test "normalize instruction" do
-  #   assert(Day5.normalize_instruction(1) == [0, 0, 0, 1])
-  #   assert(Day5.normalize_instruction(2) == [0, 0, 0, 2])
-  #   assert(Day5.normalize_instruction(3) == [0, 0, 0, 3])
-  #   assert(Day5.normalize_instruction(4) == [0, 0, 0, 4])
-  #   assert(Day5.normalize_instruction(99) == [0, 0, 0, 99])
+  test "normalize instruction" do
+    assert(Day5.normalize_instruction(1) == [0, 0, 0, 1])
+    assert(Day5.normalize_instruction(2) == [0, 0, 0, 2])
+    assert(Day5.normalize_instruction(3) == [0, 0, 0, 3])
+    assert(Day5.normalize_instruction(4) == [0, 0, 0, 4])
+    assert(Day5.normalize_instruction(99) == [0, 0, 0, 99])
 
-  #   assert(Day5.normalize_instruction(101) == [0, 0, 1, 1])
-  #   assert(Day5.normalize_instruction(1101) == [0, 1, 1, 1])
-  #   assert(Day5.normalize_instruction(11101) == [1, 1, 1, 1])
-  #   assert(Day5.normalize_instruction(10101) == [1, 0, 1, 1])
-  #   assert(Day5.normalize_instruction(11001) == [1, 1, 0, 1])
-  #   assert(Day5.normalize_instruction(10001) == [1, 0, 0, 1])
+    assert(Day5.normalize_instruction(101) == [0, 0, 1, 1])
+    assert(Day5.normalize_instruction(1101) == [0, 1, 1, 1])
+    assert(Day5.normalize_instruction(11101) == [1, 1, 1, 1])
+    assert(Day5.normalize_instruction(10101) == [1, 0, 1, 1])
+    assert(Day5.normalize_instruction(11001) == [1, 1, 0, 1])
+    assert(Day5.normalize_instruction(10001) == [1, 0, 0, 1])
 
-  #   assert(Day5.normalize_instruction(11099) == [1, 1, 0, 99])
-  #   assert(Day5.normalize_instruction(11199) == [1, 1, 1, 99])
-  #   assert(Day5.normalize_instruction(10199) == [1, 0, 1, 99])
-  #   assert(Day5.normalize_instruction(11099) == [1, 1, 0, 99])
-  #   assert(Day5.normalize_instruction(10099) == [1, 0, 0, 99])
-  # end
+    assert(Day5.normalize_instruction(11099) == [1, 1, 0, 99])
+    assert(Day5.normalize_instruction(11199) == [1, 1, 1, 99])
+    assert(Day5.normalize_instruction(10199) == [1, 0, 1, 99])
+    assert(Day5.normalize_instruction(11099) == [1, 1, 0, 99])
+    assert(Day5.normalize_instruction(10099) == [1, 0, 0, 99])
+  end
 
-  # test "preprocess input" do
-  #   assert(Day5.preprocess_input([101, 1, 1]) == [[0, 0, 1, 1], 1, 1])
-  #   assert(Day5.preprocess_input([10099, 3, 4]) == [[1, 0, 0, 99], 3, 4])
-  # end
+  test "preprocess input" do
+    assert(Day5.preprocess_input([101, 1, 1]) == [[0, 0, 1, 1], 1, 1])
+    assert(Day5.preprocess_input([10099, 3, 4]) == [[1, 0, 0, 99], 3, 4])
+  end
 
-  # test "process" do
-  #   input = [1, 0, 0, 0, 99]
-  #   pp = Day5.preprocess_input(input)
-  #   assert(Day5.process(pp, input, 0, []) == [[], [2, 0, 0, 0, 99]])
-  # end
+  test "process" do
+    input = [1, 0, 0, 0, 99]
+    pp = Day5.preprocess_input(input)
+    assert(Day5.process(pp, input, 0, []) == [[], [2, 0, 0, 0, 99]])
+  end
 
-  # test "process2" do
-  #   input = [2, 3, 0, 3, 99]
-  #   pp = Day5.preprocess_input(input)
-  #   assert(Day5.process(pp, input, 0, []) == [[], [2, 3, 0, 6, 99]])
-  # end
+  test "process2" do
+    input = [2, 3, 0, 3, 99]
+    pp = Day5.preprocess_input(input)
+    assert(Day5.process(pp, input, 0, []) == [[], [2, 3, 0, 6, 99]])
+  end
 
   test "process3" do
-    input = [5, 10, 3, 4, 2, 99]
+    input = [5, 10, 3, 4, 4, 2, 99]
     pp = Day5.preprocess_input(input)
-    assert(Day5.process(pp, input, 0, []) == [[10], [5, 10, 3, 4, 2, 99]])
+    assert(Day5.process(pp, input, 0, []) == [[3], [5, 10, 3, 4, 4, 2, 99]])
   end
 
   # test "process3" do
