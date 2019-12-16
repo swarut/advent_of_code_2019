02624492ac5c6fe08a4df8f487069b248b97962c
Warut Surapat <wsurapat@gilt.jp>
2019-12-16 18:35:24 +0900

    Start day 10.

---
+++ day10/elixir/lib/day10.ex
@@ -0,0 +1,18 @@
+defmodule Day10 do
+  @moduledoc """
+  Documentation for Day10.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Day10.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
