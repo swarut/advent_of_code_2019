f3c1aa49299012fab72eeea0c80fe8b2674ba7c0
Warut Surapat <s.warut@gmail.com>
2019-12-18 01:21:48 +0900

    Parse day 10 input.

--- day10/elixir/lib/day10.ex
+++ day10/elixir/lib/day10.ex
@@ -1,18 +1,42 @@
 defmodule Day10 do
-  @moduledoc """
-  Documentation for Day10.
-  """
+  def get_input do
+    {:ok, result} = File.read("input.txt")
 
-  @doc """
-  Hello world.
+    String.split(result, [",", "\n"], trim: true)
+    |> coordinates_from_lines
+  end
 
-  ## Examples
+  def solve_part1() do
+    get_input()
+  end
 
-      iex> Day10.hello()
-      :world
+  def coordinates_from_lines(lines) do
+    %{coordinates: coordinates} =
+      lines
+      |> Enum.reduce(
+        %{row: 0, coordinates: []},
+        fn line, %{row: row, coordinates: coordinates} = acc ->
+          new_coordinates = coordinates_from_line(line, row)
+          acc |> Map.put(:row, row + 1) |> Map.put(:coordinates, [new_coordinates | coordinates])
+        end
+      )
 
-  """
-  def hello do
-    :world
+    coordinates |> Enum.reverse() |> List.flatten()
+  end
+
+  def coordinates_from_line(line, row) do
+    extract_coordinates(line |> String.graphemes(), row, 0, [])
+  end
+
+  def extract_coordinates(["." | rest], row, col, acc) do
+    extract_coordinates(rest, row, col + 1, acc)
+  end
+
+  def extract_coordinates(["#" | rest], row, col, acc) do
+    extract_coordinates(rest, row, col + 1, [{row, col} | acc])
+  end
+
+  def extract_coordinates([], _row, _col, acc) do
+    acc |> Enum.reverse()
   end
 end
--- day10/elixir/test/day10_test.exs
+++ day10/elixir/test/day10_test.exs
@@ -2,7 +2,13 @@ defmodule Day10Test do
   use ExUnit.Case
   doctest Day10
 
-  test "greets the world" do
-    assert Day10.hello() == :world
+  test "coordinates from line" do
+    line = ".###..#"
+    assert(Day10.coordinates_from_line(line, 0) == [{0, 1}, {0, 2}, {0, 3}, {0, 6}])
+  end
+
+  test "coordinates from lines" do
+    lines = [".###..#", "......#"]
+    assert(Day10.coordinates_from_lines(lines) == [{0, 1}, {0, 2}, {0, 3}, {0, 6}, {1, 6}])
   end
 end
